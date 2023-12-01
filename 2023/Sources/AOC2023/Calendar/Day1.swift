6b7e6e8f52cdf94847075c6baa9daa01cf28f6d2
Evan Cooper <evan.cooper@rogers.com>
2023-12-01 21:07:54 +0100

    Cleanup 2023/1

--- 2023/Sources/AOC2023/Calendar/Day1.swift
+++ 2023/Sources/AOC2023/Calendar/Day1.swift
@@ -2,7 +2,7 @@ import AOCKit
 
 struct Day1: Solution {
 
-    private let digits = (0...9).map { "\($0)" }
+    private let digits = (0...9).map { Character("\($0)") }
     private let speltDigits = [
         "one": "1",
         "two": "2",
@@ -16,63 +16,49 @@ struct Day1: Solution {
     ]
 
     func question1() -> Any {
-        var sum = 0
-        
         func number(in line: String) -> String? {
             for character in line {
-                let stringCharacter = String(character)
-                if digits.contains(stringCharacter) {
-                    return stringCharacter
+                if digits.contains(character) {
+                    return String(character)
                 }
             }
             return nil
         }
 
-        for line in lines(for: "day1", in: .module) {
+        return lines(for: "day1", in: .module).reduce(into: 0) { partialSum, line in
             let first = number(in: line)
             let last = number(in: String(line.reversed()))
 
-            guard let first, let last, let number = Int(first + last) else { continue }
-            sum += number
+            if let first, let last, let number = Int(first + last) {
+                partialSum += number
+            }
         }
-
-        return sum
     }
 
     func question2() -> Any {
-        var sum = 0
-
         func number(in line: String, reversed: Bool) -> String? {
-            var buffer = ""
+            var buffer = [Character]()
             for character in line {
-                let stringCharacter = String(character)
-                if digits.contains(stringCharacter) {
-                    return stringCharacter
+                if digits.contains(character) {
+                    return String(character)
                 } else {
-                    buffer += String(character)
-                    if reversed {
-                        let reversedBuffer = String(buffer.reversed())
-                        if let match = speltDigits.first(where: { key, _ in reversedBuffer.contains(key) }) {
-                            return match.value
-                        }
-                    } else {
-                        if let match = speltDigits.first(where: { key, _ in buffer.contains(key) }) {
-                            return match.value
-                        }
+                    buffer.append(character)
+                    let stringBuffer = reversed ? String(buffer.reversed()) : String(buffer)
+                    if let match = speltDigits.first(where: { key, _ in stringBuffer.contains(key) }) {
+                        return match.value
                     }
                 }
             }
             return nil
         }
 
-        for line in lines(for: "day1", in: .module) {
+        return lines(for: "day1", in: .module).reduce(into: 0) { partialSum, line in
             let first = number(in: line, reversed: false)
             let last = number(in: String(line.reversed()), reversed: true)
 
-            guard let first, let last, let number = Int(first + last) else { continue }
-            sum += number
+            if let first, let last, let number = Int(first + last) {
+                partialSum += number
+            }
         }
-
-        return sum
     }
 }
