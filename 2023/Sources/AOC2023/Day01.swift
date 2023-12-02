236e0019ebb2922a0682a76111570d9e04be2dd0
Evan Cooper <evan.cooper@rogers.com>
2023-12-02 13:15:47 +0100

    more cleanup (I don't know when to stop)

--- 2023/Sources/AOC2023/Day01.swift
+++ 2023/Sources/AOC2023/Day01.swift
@@ -1,22 +1,7 @@
 import AOCKit
 
 struct Day01: Solution {
-    
-    private enum Constants {
-        static let speltDigits = [
-            "zero": 0,
-            "one": 1,
-            "two": 2,
-            "three": 3,
-            "four": 4,
-            "five": 5,
-            "six": 6,
-            "seven": 7,
-            "eight": 8,
-            "nine": 9
-        ]
-    }
-    
+
     func question1() -> Any {
         result { line, _ in
             line.compactMap(\.wholeNumberValue).first
@@ -24,7 +9,8 @@ struct Day01: Solution {
     }
 
     func question2() -> Any {
-        result { line, reversed in
+        let speltDigits = ["zero": 0, "one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9]
+        return result { line, reversed in
             var buffer = [Character]()
             for character in line {
                 if let number = character.wholeNumberValue {
@@ -32,7 +18,7 @@ struct Day01: Solution {
                 } else {
                     buffer.append(character)
                     let stringBuffer = reversed ? String(buffer.reversed()) : String(buffer)
-                    if let match = Constants.speltDigits.first(where: { key, _ in stringBuffer.contains(key) }) {
+                    if let match = speltDigits.first(where: { key, _ in stringBuffer.contains(key) }) {
                         return match.value
                     }
                 }
@@ -45,8 +31,8 @@ struct Day01: Solution {
         input.reduce(into: 0) { partialSum, line in
             let first = lineHandler(line, false)
             let second = lineHandler(String(line.reversed()), true)
-            if let first, let second, let number = Int("\(first)\(second)") {
-                partialSum += number
+            if let first, let second {
+                partialSum += (first * 10) + second
             }
         }
     }
--- 2023/Sources/AOC2023/main.swift
+++ 2023/Sources/AOC2023/main.swift
@@ -11,17 +11,16 @@ extension Solution {
 let day = Day02()
 print("Running: \(type(of: day))")
 
-func execute(function: () -> Any) {
+func run(_ function: () -> Any) {
     let start = CFAbsoluteTimeGetCurrent()
     let ans = function()
     let diff = CFAbsoluteTimeGetCurrent() - start
-
     print("Answer:", ans)
     print("Ran in:", diff, "s")
 }
 
 print("== Question 1 ==")
-execute(function: day.question1)
+run(day.question1)
 
 print("== Question 2 ==")
-execute(function: day.question2)
+run(day.question2)
