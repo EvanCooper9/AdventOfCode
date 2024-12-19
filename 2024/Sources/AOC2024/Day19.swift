593fe483fca93a048a62051b260f487c8a3fa2f8
Evan Cooper <evan.cooper@rogers.com>
2024-12-19 11:16:33 -0500

    2024/19

--- 2024/Sources/AOC2024/Day19.swift
+++ 2024/Sources/AOC2024/Day19.swift
@@ -1,12 +1,45 @@
 import AOCKit
 import Algorithms
+import Foundation
 
 final class Day19: Solution {
+    
+    override init(bundle: Bundle) {
+        super.init(bundle: bundle)
+        patterns = Set(input[0].split(separator: ", "))
+        patternMax = patterns.map(\.count).max() ?? 0
+    }
+    
     override func question1() -> Any {
-        fatalError()
+        input[2...]
+            .filter { possibilities(design: $0) > 0 }
+            .count
     }
     
     override func question2() -> Any {
-        fatalError()
+        input[2...]
+            .map(possibilities(design:))
+            .sum
+    }
+    
+    private var patterns = Set<Substring>()
+    private var patternMax = 0
+    private var possibilitiesCache = [String: Int]()
+    
+    private func possibilities(design: String) -> Int {
+        if let cached = possibilitiesCache[design] {
+            return cached
+        }
+        
+        let result = (0 ..< min(patternMax, design.count))
+            .map { i in
+                let sub = design[...i]
+                guard patterns.contains(sub) else { return 0 }
+                return design.count == sub.count ? 1 : possibilities(design: String(design[sub.count...]))
+            }
+            .sum
+        
+        possibilitiesCache[design] = result
+        return result
     }
 }
--- 2024/Sources/AOC2024/main.swift
+++ 2024/Sources/AOC2024/main.swift
@@ -20,7 +20,7 @@ extension Solution {
 }
 
 let start = CFAbsoluteTimeGetCurrent()
-let day = Day18(bundle: .module)
+let day = Day19(bundle: .module)
 let diff = CFAbsoluteTimeGetCurrent() - start
 print("Loaded: \(type(of: day)) in:", diff, "s")
 print("Running: \(type(of: day))")
